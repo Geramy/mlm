d31c358af244fc06835c20dfbf792896f7c9a875
geramyl <gloveless@jqluv.com>
2020-05-23 20:15:03 -0700

    included in html

--- lib/MLM/Gallery/Model.pm
+++ lib/MLM/Gallery/Model.pm
@@ -24,7 +24,7 @@ sub edit {
 FROM product_gallery
 INNER JOIN member m ON m.memberid = ?
 LEFT JOIN def_type t ON t.typeid = m.typeid
-WHERE galleryid=?", $self->$ARGS->{memberid}, $self->{ARGS}->{galleryid});
+WHERE galleryid=?", $self->{ARGS}->{memberid}, $self->{ARGS}->{galleryid});
   return $err if $err;
   $self->{ARGS}->{discount_price} = $self->{ARGS}->{price} / 100 * $self->{ARGS}->{pd} + $self->{ARGS}->{price};
   return $self->SUPER::edit(@_);
