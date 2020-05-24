f771a850cbc04f3bcab330b965d244a9b7388151
geramyl <gloveless@jqluv.com>
2020-05-24 07:44:01 -0700

    fixing ambigious columns again.

--- lib/MLM/Basket/Model.pm
+++ lib/MLM/Basket/Model.pm
@@ -13,8 +13,8 @@ sub topics {
 
 	$self->{LISTS} = [];
 	my $err = $self->select_sql($self->{LISTS},
-"SELECT basketid, classify, id, title, logo, (price - ((price / 100) * t.product_discount)), sh, bv, qty,
-	(qty*(price - ((price / 100) * t.product_discount))) AS amount, (qty*bv) AS credit, (qty*sh) AS shipping, (price - ((price / 100) * t.product_discount)) as discount_price
+"SELECT basketid, classify, id, title, logo, (b.price - ((price / 100) * t.product_discount)), sh, bv, qty,
+	(qty*(b.price - ((b.price / 100) * t.product_discount))) AS amount, (qty*bv) AS credit, (qty*sh) AS shipping, (b.price - ((price / 100) * t.product_discount)) as discount_price
 FROM sale_basket b
 INNER JOIN product_gallery g ON (b.id=g.galleryid AND b.classify='gallery')
 INNER JOIN member m ON m.memberid = ?
@@ -22,10 +22,12 @@ INNER JOIN def_type t ON t.typeid = m.typeid
 WHERE memberid=? AND inbasket='Yes'
 UNION
 SELECT basketid, classify, id, title, logo, (price - ((price / 100) * t.product_discount)), sh, bv, qty,
-	(qty*(price - ((price / 100) * t.product_discount))) AS amount, (qty*bv) AS credit, (qty*sh) AS shipping
+	(qty*(price - ((b.price / 100) * t.product_discount))) AS amount, (qty*bv) AS credit, (qty*sh) AS shipping
 FROM sale_basket b
+INNER JOIN member m ON m.memberid = ?
+INNER JOIN def_type t ON t.typeid = m.typeid
 INNER JOIN product_package g ON (b.id=g.packageid AND b.classify='package')
-WHERE memberid=? AND inbasket='Yes'",  $ARGS->{memberid}, $ARGS->{memberid}, $ARGS->{memberid});
+WHERE memberid=? AND inbasket='Yes'",  $ARGS->{memberid}, $ARGS->{memberid}, $ARGS->{memberid}, $ARGS->{memberid});
   if($self->{CUSTOM}->{StripeAPIPubKey}) {
     $ARGS->{StripeAPIPubKey} = $self->{CUSTOM}->{StripeAPIPubKey};
   }
